b92e68542242007d58d3bdc56dc1f76f0c3cdb5f
tomasmali <megatoys92@gmail.ocm>
2019-12-07 18:07:40 +0100

    funzionante

--- src/app/sanshi/sanshi.component.ts
+++ src/app/sanshi/sanshi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sanshi',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SanshiComponent implements OnInit {
 
-  constructor() { }
+  response: any;
+
+  allUsers: []
+
+  constructor(public http: HttpClient) { 
+    this.http
+    .get('http://localhost:3000/users/').subscribe((response)=> {
+      console.log(response)
+      
+      this.response = response
+
+    })
+  }
 
   ngOnInit() {
+
   }
+
+
+
+getTables(){
+  this.http
+    .get('http://localhost:3000/users/').subscribe((response)=> {
+      console.log(response)
+      this.response = response
+    })
+}
+
+
+
 
 }
